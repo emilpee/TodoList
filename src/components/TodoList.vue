3d641ccbe2be7fb131392fd1c19532c7789b9e47
Emil Petersson <emilpet93@gmail.com>
2018-11-26 13:29:35 +0100

    New functions to add and remove tasks

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,18 @@
 <template>
+<div class="container">
   <div class="list">
     <h1>To Do List</h1>
     <ul>
-      <li v-for="task in tasks" v-text="task.task" :key="task.id"> </li>
+      <li v-for="task in tasks" :key="task.id"> 
+        <input type="checkbox">
+        <span> {{ task.task }} </span>
+        <button @click="remove(task)">x</button>
+      </li>
     </ul>
   </div>
+  <input type="text" v-model="input.task" placeholder="I need to...">
+  <button type="button" @click="add">Add task</button>
+</div>
 </template>
 
 <script>
@@ -13,6 +21,17 @@ export default {
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    },
+    input() {
+      return this.$store.state.input;
+    }
+  },
+  methods: {
+    add() {
+      this.$store.commit('add');
+    },
+    remove(task) {
+      this.$store.commit('remove', task);
     }
   }
 }
@@ -20,18 +39,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1 {
+  font-size: 42px;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
 }
 a {
   color: #42b983;
+}
+.list {
+  border: 15px goldenrod groove;
+  border-radius: 20px;
+  background-color: #fff6a2;
+  width: 70%;
+  margin: 0 auto;
+  margin-bottom: 10px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,13 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    input: {},
     tasks: [
       {
         task: "This is a task",
         check: false
-      },
+      }
     ]
   },
   mutations: {
+    add: function(state) {
+      state.tasks.push({
+        task: state.input.task,
+        check: false
+      });
+      state.input.task = ''; // Reset the input field
+    },
+    remove(state, task) {
+      state.tasks.splice(state.tasks.indexOf(task), 1);
+    }
   },
 })
